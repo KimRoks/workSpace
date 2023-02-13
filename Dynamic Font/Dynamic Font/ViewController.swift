4ea6518016db25d811535d7185373783a2f3ba34
KimRoks <rudfhr3161@naver.com>
2023-02-13 11:12:21 +0900

    화면 크기에 따른 텍스트의 동적 변화 실습

---
+++ Dynamic Font/Dynamic Font/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dynamic Font
+//
+//  Created by 김경록 on 2023/02/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
